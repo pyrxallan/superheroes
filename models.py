7144cad87bed6bb15c997529a811ab104da1a0d3
Allan Ratemo <allanisratemo@gmail.com>
2026-01-08 07:12:18 +0300

    Initial commit

--- models.py
+++ models.py
@@ -6,36 +6,6 @@ class Hero(db.Model):
     __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    super_name = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    # Relationship
-    hero_powers = db.relationship('HeroPower', back_populates='hero')
-
-class Power(db.Model):
-    __tablename__ = 'powers'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    # Relationship
-    hero_powers = db.relationship('HeroPower', back_populates='power')
-
-class HeroPower(db.Model):
-    __tablename__ = 'hero_powers'
-
-    id = db.Column(db.Integer, primary_key=True)
-    strength = db.Column(db.String(20), nullable=False)
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    # Relationships
-    hero = db.relationship('Hero', back_populates='hero_powers')
-    power = db.relationship('Power', back_populates='hero_powers')
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
+    powers = db.relationship('Power', secondary='hero_powers', back_populates='heroes')
